5945b44bfa5c94ec0051d3e694036cf189b16bf1
Kunal Singh <66094420+94kunal@users.noreply.github.com>
2023-05-14 23:30:25 +0200

    Add files via upload
    Basic Testing of Logic

---
+++ TestingLogic.py
@@ -0,0 +1,33 @@
+from LogicFile import *
+mat = start_game()
+print(mat)
+
+add_new_2(mat)
+print(mat)
+
+add_new_2(mat)
+print(mat)
+
+mat = move_up(mat)
+print(mat)
+
+mat = move_right(mat)
+print(mat)
+
+add_new_2(mat)
+print(mat)
+
+mat = move_down(mat)
+print(mat)
+
+add_new_2(mat)
+print(mat)
+
+mat = move_left(mat)
+print(mat)
+
+mat = move_down(mat)
+print(mat)
+
+mat = move_left(mat)
+print(mat)
